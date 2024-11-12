27e1e4a0cfe012756a817369faa4c83126acf7db
TomaszHajas <tomaszhajas@gmail.com>
2024-11-12 20:46:19 +0100

    Update index.ts

--- ai/index.ts
+++ ai/index.ts
@@ -1,11 +1,18 @@
-import { openai } from '@ai-sdk/openai';
+//import { openai } from '@ai-sdk/openai';
+const { G4F } = require("g4f");
+
 import { experimental_wrapLanguageModel as wrapLanguageModel } from 'ai';
 
 import { customMiddleware } from './custom-middleware';
 
-export const customModel = (apiIdentifier: string) => {
-  return wrapLanguageModel({
-    model: openai(apiIdentifier),
-    middleware: customMiddleware,
-  });
-};
+//export const customModel = (apiIdentifier: string) => {
+  //return wrapLanguageModel({
+    //model: openai(apiIdentifier),
+    //middleware: customMiddleware,
+  //});
+//};
+export const customModel = new G4F();
+const messages = [
+    { role: "user", content: "Hi, what's up?"}
+];
+customModel.chatCompletion(messages).then(console.log);
