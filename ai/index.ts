34e001d9fc438c521e06f05a9a9b94d33d3a4aca
TomaszHajas <tomaszhajas@gmail.com>
2024-11-14 00:04:44 +0100

    Update index.ts

--- ai/index.ts
+++ ai/index.ts
@@ -5,19 +5,36 @@ import { experimental_wrapLanguageModel as wrapLanguageModel } from 'ai';
 
 import { customMiddleware } from './custom-middleware';
 
-//export const customModel = (apiIdentifier: string) => {
-  //return wrapLanguageModel({
-    //model: openai(apiIdentifier),
-    //middleware: customMiddleware,
-  //});
-//};
-export const g4f = new G4F();
-const messages = [
-    { role: "user", content: "Hi, what's up?"}
-];
-console.log("test");
-g4f.chatCompletion(messages).then(console.log);
 
+//--- TODO custom handler
+const { G4F } = require("g4f");
+const g4f = new G4F();
+const messages = [
+    { role: "system", content: "You're an expert bot in poetry."},
+    { role: "user", content: "Let's see, write a single paragraph-long poem for me." },
+];
+const options = {
+    model: "gpt-4",
+    debug: true,
+	retry: {
+        times: 3,
+        condition: (text) => {
+            const words = text.split(" ");
+            return words.length > 10;
+        }
+    },
+    output: (text) => {
+        return text + " 💕🌹";
+    }
+};
+
+console.log("TEST");
+(async() => {
+    const text = await g4f.chatCompletion(messages, options);	
+    console.log(text);
+})();
+
+//---
 export const customModel = (apiIdentifier: string) => {
   return wrapLanguageModel({
     model: openai(apiIdentifier),
